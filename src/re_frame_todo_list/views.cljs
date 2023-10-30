5f25dc53f94be3f4e4fe290f1b5a40b6df93cd16
Tanner Hoelzel <thoelze1@binghamton.edu>
2023-10-30 13:24:01 -0300

    import and use react-beautiful-dnd

--- src/re_frame_todo_list/views.cljs
+++ src/re_frame_todo_list/views.cljs
@@ -5,8 +5,8 @@
    [re-com.core   :refer [at h-box v-box box gap line label title slider checkbox input-text horizontal-bar-tabs vertical-bar-tabs p]]
    [re-com.slider :refer [slider-parts-desc slider-args-desc]]
    [reagent.core  :as    reagent]
+   ["react-beautiful-dnd" :as react-beautiful-dnd]
    [re-frame-todo-list.macros :as macros]
-   [cljsjs.react-flip-move]
    ))
 
 ;; the todo items should comprise a priority level; therefore this priority
@@ -15,18 +15,12 @@
 ;; visual abstraction. therefore the height related information should live in
 ;; the view context, and perhaps passed as necessary to the events context
 
-
 ; https://stackoverflow.com/questions/33446913/reagent-react-clojurescript-warning-every-element-in-a-seq-should-have-a-unique
 ; https://stackoverflow.com/questions/24239144/js-console-log-in-clojurescript
 
-(def flip-move (reagent/adapt-react-class js/FlipMove))
-
-(def initial 0)
-(def multiple 60)
-
-(defn idx->height
-  [idx]
-  (+ initial (* multiple idx)))
+(def drag-drop-context (reagent/adapt-react-class react-beautiful-dnd/DragDropContext))
+(def droppable (reagent/adapt-react-class react-beautiful-dnd/Droppable))
+(def draggable (reagent/adapt-react-class react-beautiful-dnd/Draggable))
 
 (defn item-view
   [index item]
@@ -56,70 +50,22 @@
      (if (= 0 (count @items))
        [:div "You've got nothing to do!"]
        [:div.container
-        {:style {:position :relative}}
-        ;; This was an attempt to re-draw the dragged element! Makes much more
-        ;; sense to directly manipulate the element drawn by flip-move
-        (if @selected-item
-          (do #_(println "item: " (str (js/document.getElementById (str @selected-item))))
-              (let [h (-> js/document
-                          (.getElementById (str (get-in @items [@selected-item :id])))
-                          (.getBoundingClientRect)
-                          (.-height))]
-                (do #_(println (str h))
-                    [:div 
-                     (let [item (get @items @selected-item)]
-                       [:div.row {:style {
-                           ;:position :absolute
-                           :flex-wrap "wrap"
-                           ;:display "flex"
-                           ;:flex-direction "row"
-                                          }}
-                        [:div.row {:style {:position :absolute}}
-                         [:div.row {:style {:position :absolute
-                                            :top (:height item)
-                                            :z-index 1}}
-                          [item-view @selected-item item]]]])]))))
-        (if (and (not @selected-item) @moving?)
-          [:div
-           [:div.row {:style {:flex-wrap "wrap"}}
-            [:div.row {:style {:position :absolute}}
-             [:div.row {:style {:position :absolute
-                                :top (:height (get @items @moving?))
-                                :z-index 1}}
-              [item-view @selected-item (get @items @moving?)]]]]])
-        
-        [flip-move {:duration 500
-                    :easing "cubic-bezier(0, 1, 1, 1)"
-                    :onStartAll (fn [reactKids domNodes]
-                                  (do (js/console.log "start" @moving?)
-                                      (reset! moving? @selected-item)))
-                    :onFinishAll (fn [reactKids domNodes]
-                                   (do (js/console.log "stop" @moving?)
-                                       (reset! moving? nil)))}
-         (doall
-          (map-indexed
-           (fn [index item]
-             ^{:key (:id item)}
-             [:div.row
-              
-              {:style {:visibility (if (or (= index @selected-item)
-                                           (= index @moving?)) :hidden)
-                       ;;:position (if (= index @selected-item) :absolute)
-                       ;; if we manually track all the heights, then that means
-                       ;; that deletion becomes a constant time operation as we
-                       ;; have to manually go through and change all the
-                       ;; subsequent heights. Instead, we'll specially render the
-                       ;; dragged item and the displaced item. For now we'll store
-                       ;; the height information in the items list, though we
-                       ;; could store it directly in the app-db
-                       :top (if (= index @selected-item)
-                                     (:height item)
-                                     0 ;(idx->height index)
-                                     )}
-               :key (:id item)
-               :id (str (:id item))}
-              [item-view index item]])
-           @items))]])])))
+        ;; Example droppable (wraps one of your lists)
+        ;; Note use of r/as-element and js->clj on droppableProps
+        #_[droppable {:droppable-id "droppable-1" :type "thing"}
+         (fn [provided snapshot]
+           (reagent/as-element [:div (merge {:ref   (.-innerRef provided)
+                                             :class (when (.-isDraggingOver snapshot) :drag-over)}
+                                            (js->clj (.-droppableProps provided)))
+                                [:h2 "My List - render some draggables inside"]
+                                (.-placeholder provided)]))]
+                                        ; Example draggable
+        #_[draggable {:draggable-id "draggable-1", :index 0}
+         (fn [provided snapshot]
+           (reagent/as-element [:div (merge {:ref (.-innerRef provided)}
+                                            (js->clj (.-draggableProps provided))
+                                            (js->clj (.-dragHandleProps provided)))
+                                [:p "Drag me"]]))]])])))
 
 (defn item-input
   []
@@ -142,111 +88,27 @@
        :value "Add item"
        :on-click add-item}]]))
 
-
-(defn slider-demo
-  []
-  (let [slider-val  (reagent/atom "0")
-        slider-min  (reagent/atom "0")
-        slider-max  (reagent/atom "100")
-        slider-step (reagent/atom "1")
-        disabled?   (reagent/atom false)]
-    (fn
-      []
-      [:div
-       [slider
-        :src       (at)
-        :model     slider-val
-        :min       slider-min
-        :max       slider-max
-        :step      slider-step
-        :width     "300px"
-        :on-change #(reset! slider-val (str %))
-        :disabled? disabled?]
-       [:div
-        [title :src (at) :level :level3 :label "Interactive Parameters" :style {:margin-top "0"}]
-        [h-box
-           :src      (at)
-           :gap      "10px"
-           :align    :center
-           :children [[box
-                       :src      (at)
-                       :align :start
-                       :width "60px"
-                       :child [:code ":val"]]
-                      [input-text
-                       :src      (at)
-                       :model           slider-val
-                       :width           "60px"
-                       :height          "26px"
-                       :on-change       #(reset! slider-val %)
-                       :change-on-blur? false]]]
-        [h-box
-           :src      (at)
-           :gap      "10px"
-           :align    :center
-           :children [[box
-                       :src      (at)
-                       :align :start
-                       :width "60px"
-                       :child [:code ":min"]]
-                      [input-text
-                       :src      (at)
-                       :model           slider-min
-                       :width           "60px"
-                       :height          "26px"
-                       :on-change       #(reset! slider-min %)
-                       :change-on-blur? false]]]
-        [h-box
-           :src      (at)
-           :gap      "10px"
-           :align    :center
-           :children [[box
-                       :src      (at)
-                       :align :start
-                       :width "60px"
-                       :child [:code ":max"]]
-                      [input-text
-                       :src      (at)
-                       :model           slider-max
-                       :width           "60px"
-                       :height          "26px"
-                       :on-change       #(reset! slider-max %)
-                       :change-on-blur? false]]]
-        [h-box
-           :src      (at)
-           :gap      "10px"
-           :align    :center
-           :children [[box
-                       :src      (at)
-                       :align :start
-                       :width "60px"
-                       :child [:code ":step"]]
-                      [input-text
-                       :src      (at)
-                       :model           slider-step
-                       :width           "60px"
-                       :height          "26px"
-                       :on-change       #(reset! slider-step %)
-                       :change-on-blur? false]]]
-        [checkbox
-         :src      (at)
-         :label [box
-                 :src      (at)
-                 :align :start :child [:code ":disabled?"]]
-         :model disabled?
-         :on-change (fn [val]
-                      (reset! disabled? val))]]])))
-
-
 (defn main-panel []
   (let [items (re-frame/subscribe [::subs/items])
         selected-item (re-frame/subscribe [::subs/selected-item])
         si @selected-item
         new-item (re-frame/subscribe [::subs/new-item])]
-    [:div.container
-     [:div.row]
-     #_[:div.row
-      [:p (str @selected-item)]
-      [:p (str @items)]]
-     [item-input]
-     [items-view]]))
+    ;; Example drag-drop-context (typically wraps your whole app)
+    [drag-drop-context
+     {:onDragStart  #()
+      :onDragUpdate #()
+      :onDragEnd    #()}
+     [:div
+      [droppable {:droppable-id "droppable-1" :type "thing"}
+       (fn [provided snapshot]
+         (reagent/as-element [:div (merge {:ref   (.-innerRef provided)
+                                     :class (when (.-isDraggingOver snapshot) :drag-over)}
+                                    (js->clj (.-droppableProps provided)))
+                        [:h2 "My List - render some draggables inside"
+                         [draggable {:draggable-id "draggable-1", :index 0}
+                          (fn [provided snapshot]
+                            (reagent/as-element [:div (merge {:ref (.-innerRef provided)}
+                                                       (js->clj (.-draggableProps provided))
+                                                       (js->clj (.-dragHandleProps provided)))
+                                           [:p "Drag me"]]))]]
+                        (.-placeholder provided)]))]]]))
