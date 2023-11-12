c816365665ac071a6a006df01b0728ec6ed9e619
Tanner Hoelzel <thoelze1@binghamton.edu>
2023-11-11 22:03:16 -0500

    start an http server (aleph)
    https://www.youtube.com/watch?v=6jvG3XbSeos

---
+++ src/clj/web/core.clj
@@ -0,0 +1,18 @@
+(ns web.core
+  (:require [reitit.ring :as ring]
+            [muuntaja.core :as m]
+            [aleph.http :as http]
+            [mount.core :as mount]))
+
+(def app
+  (ring/ring-handler
+   (ring/router
+    [["/"]]
+    {:data {:muuntaja m/instance}})))
+
+(mount/defstate server
+  :start (http/start-server #'app {:port 4080})
+  :stop (.stop server))
+
+(defn -main [& _]
+  (mount/start))
