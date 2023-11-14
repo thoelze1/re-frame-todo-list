9ede2153cba8404198e1bd276e006a953c6b46b9
Tanner Hoelzel <thoelze1@binghamton.edu>
2023-11-14 11:33:26 -0500

    use persistent sqlite database

--- src/clj/database/core.clj
+++ src/clj/database/core.clj
@@ -2,8 +2,16 @@
   (:require [xtdb.api :as xt]
             [mount.core :as mount]))
 
+(def connection-pool
+  {:xtdb.jdbc/connection-pool {:dialect {:xtdb/module 'xtdb.jdbc.sqlite/->dialect}
+                               :db-spec {:dbname "test"}}
+   :xtdb/tx-log {:xtdb/module 'xtdb.jdbc/->tx-log
+                 :connection-pool :xtdb.jdbc/connection-pool}
+   :xtdb/document-store {:xtdb/module 'xtdb.jdbc/->document-store
+                         :connection-pool :xtdb.jdbc/connection-pool}})
+
 (mount/defstate node
-  :start (xt/start-node {})
+  :start (xt/start-node connection-pool)
   :stop (.close node))
 
 (defn ensure-id [{:keys [:xt/id] :as m}]
@@ -18,6 +26,7 @@
     (xt/submit-tx node [[::xt/put with-id]])))
 
 (write {:data "Hi"})
+
 #_(defn query []
     (crux/q
      (crux/db node)
