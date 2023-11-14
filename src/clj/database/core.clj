e1ba8996f722cb89d91b71efeb7630ebad3c18ed
Tanner Hoelzel <thoelze1@binghamton.edu>
2023-11-14 11:04:52 -0500

    update xtdb dependency

--- src/clj/database/core.clj
+++ src/clj/database/core.clj
@@ -1,25 +1,25 @@
 (ns database.core
-  (:require [crux.api :as crux]
+  (:require [xtdb.api :as xt]
             [mount.core :as mount]))
 
 (mount/defstate node
-  :start (crux/start-node {}))
+  :start (xt/start-node {})
+  :stop (.close node))
 
-(defn ensure-id [{:keys [:crux.db/id] :as m}]
+(defn ensure-id [{:keys [:xt/id] :as m}]
   (if-not id
-    (assoc m :crux.db/id (java.util.UUID/randomUUID))
+    (assoc m :xt/id (java.util.UUID/randomUUID))
     m))
 
-(ensure-id {:crux.db/id 1234})
+(ensure-id {:xt/id 1234})
 
 (defn write [entity]
   (let [with-id (ensure-id entity)]
-    (crux/submit-tx node [[:crux.tx/put with-id]])))
+    (xt/submit-tx node [[::xt/put with-id]])))
 
-(defn query []
-  (crux/q
-   (crux/db node)
-   '{:find [(pull ?e [*])]
-     :where [[?e :crux.db/id _]] }))
-
-(query)
+(write {:data "Hi"})
+#_(defn query []
+    (crux/q
+     (crux/db node)
+     '{:find [(pull ?e [*])]
+       :where [[?e :crux.db/id _]] }))
